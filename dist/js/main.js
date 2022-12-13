9184bacca0127bfc3bdfe798471a06a8a0dad866
Rahma-saltni <saltnirahma9@gmail.com>
2022-12-13 14:27:02 +0100

    lesson 10

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,2 @@
-const myMap=new Map,mySet=new Set;
+$((function(){"use strict";$(".toggle-sidebar").on("click",(function(){$(".content-area, .sidebar").toggleClass("no-sidebar")}))}));const myMap=new Map,mySet=new Set;
 //# sourceMappingURL=main.js.map
